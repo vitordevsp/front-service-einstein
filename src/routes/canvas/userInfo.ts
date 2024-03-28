8a724337950fe3ccb342ea3e5016b5b6f1bd6cc5
vitordevsp <contatodevsp@gmail.com>
2024-03-28 19:37:44 -0300

    feat: implementa a logica no endpoint canvas/userInfo

--- src/routes/canvas/userInfo.ts
+++ src/routes/canvas/userInfo.ts
@@ -1,9 +1,17 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify"
+import { canvasApi } from "../../services/canvasApi"
 
 export async function userInfoRoute(app: FastifyInstance) {
-  app.get('/canvas/user-info', (req: FastifyRequest, reply: FastifyReply) => {
-    return reply.status(200).send({
-      ok: true,
-    })
+  app.get('/canvas/user-info/:userId', (req: FastifyRequest, reply: FastifyReply) => {
+    try {
+      const userId = (req.params as any).userId
+
+      const userInfo = canvasApi.getUser(userId)
+
+      return reply.status(200).send(userInfo)
+    }
+    catch (error) {
+      return reply.status(500).send({ error })
+    }
   })
 }
