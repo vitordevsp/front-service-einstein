117175aba52b263bf0894522b149fc765c883521
vitordevsp <contatodevsp@gmail.com>
2024-04-24 19:36:42 -0300

    feat: implementa o token do backend code

--- src/services/CoreService/index.ts
+++ src/services/CoreService/index.ts
@@ -1,20 +1,16 @@
-import { canvasAPI } from "../../lib/canvasAPI"
 import { coreAPI } from "../../lib/coreAPI"
-import { generateUserDisplayName } from "../../utils/helpers"
-import { IUserCanvasLMS } from "../../types/canvasLMS"
-import { ICanvasUserResponse } from "../../types/canvas"
 import { ICreateUserPayload, ISlidingToken } from "../../types/coreAPI"
 
 export const coreService = {
   /** Retorna informações de um usuario pelo ID. */
-  createUser: async (userPayload: ICreateUserPayload): Promise<boolean> => {
+  createUser: async (userPayload: ICreateUserPayload): Promise<ISlidingToken | null> => {
     try {
-      await coreAPI.post<ISlidingToken>('/chattutor/api/v1/auth/account', userPayload)
-      return true
+      const { data } = await coreAPI.post<ISlidingToken>('/chattutor/api/v1/auth/account', userPayload)
+      return data
     }
     catch (error) {
       console.log('ERROR | coreService.createUser: ', error)
-      return false
+      return null
     }
   },
 
