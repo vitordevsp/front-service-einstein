cdce0ae47e32415ce40d03c691d75a7cd6bd849e
vitordevsp <contatodevsp@gmail.com>
2024-05-09 15:51:45 -0300

    refactor: ajuste na rota /generator/load-lti

--- src/routes/generator/loadLTI.ts
+++ src/routes/generator/loadLTI.ts
@@ -7,7 +7,7 @@ export async function generatorLoadLTIRoute(app: FastifyInstance) {
       const userId = (req.body as any).custom_canvas_user_id.value
       const courseId = (req.body as any).custom_canvas_course_id.value
 
-      const baseUrl = process.env.CHATBOT_TUTOR_URL
+      const baseUrl = process.env.QUESTION_GENERATOR_URL
       const urlRedirect = `${baseUrl}?user_id=${userId}&course_id=${courseId}`
 
       return reply.redirect(301, urlRedirect)
